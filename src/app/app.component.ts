9a454d9e0ef2631e93b35120010d5b902a78905f
Leo Oli <leooli_artist@outlook.com>
2022-12-23 15:55:58 -0300

    4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
+  <app-diretivas-estruturais></app-diretivas-estruturais>
     <app-data-biding></app-data-biding>
     <router-outlet></router-outlet>
   `
--- src/app/data-biding/data-biding.component.ts
+++ src/app/data-biding/data-biding.component.ts
@@ -6,19 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./data-biding.component.scss']
 })
 export class DataBidingComponent implements OnInit {
-  public nome:string = "Leo Oli";
-  public idade:number = 27;
+  public nome: string = "Leo Oli";
+  public idade: number = 27;
 
   public checkedDisabled: boolean = true;
   public imgSrc: string = "https://i.pinimg.com/736x/3d/14/bb/3d14bb74a91f80906190fda62f4a57f3--creative-resume-templates-neko-kawaii.jpg";
-  public imgTitle: string ="Camomilo"
+  public imgTitle: string = "Camomilo"
 
-  constructor() {}
+  public position: {x: number, y: number} = {x: 0, y: 0}
+
+  constructor() { }
 
   ngOnInit(): void { }
 
-  public alertaInfo() {
-    alert("Deu Bom!");
+  public alertaInfo(valor: MouseEvent) {
+    alert(valor);
+  }
+
+  public mouseMoveTeste(valor: MouseEvent) {
+    this.position.x = valor.offsetX;
+    this.position.y = valor.offsetY;
+
   }
 
 }
