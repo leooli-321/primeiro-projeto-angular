e88c4246a41cfbe2dacebcc9082beb1916e95898
Leo Oli <leooli_artist@outlook.com>
2023-01-29 04:22:24 -0300

    kkk

--- src/app/diretivas-estruturais/diretivas-estruturais.component.ts
+++ src/app/diretivas-estruturais/diretivas-estruturais.component.ts
@@ -3,30 +3,45 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-diretivas-estruturais',
   templateUrl: './diretivas-estruturais.component.html',
-  styleUrls: ['./diretivas-estruturais.component.scss']
+  styleUrls: ['./diretivas-estruturais.component.scss'],
 })
 export class DiretivasEstruturaisComponent implements OnInit {
-
   public condition: boolean = true;
-
   public conditionClick: boolean = true;
 
-  constructor() { }
-  ngOnInit(): void {
+  public list: Array<{ nome: string; idade: number }> = [
+    { nome: 'Leo Oli', idade: 27 },
+    { nome: 'Athena', idade: 6 },
+    { nome: 'Hades', idade: 6 },
+    { nome: 'Sagira', idade: 4 },
+  ];
 
+  constructor() {}
+  ngOnInit(): void {
     setInterval(() => {
       if (this.condition) {
         this.condition = false;
-      } else { this.condition = true; }
-
-    }, 2000)
-
+      } else {
+        this.condition = true;
+      }
+    }, 2000);
   }
 
-  public onClick(){
+  public onClick() {
     if (this.conditionClick) {
       this.conditionClick = false;
-    } else { this.conditionClick = true; }
+    } else {
+      this.conditionClick = true;
+    }
+  }
+
+  public onClickAddList() {
+    this.list.push({ nome: 'Maira', idade: 19 });
+  }
+
+  public onClickEventList(event: number) {
+
+    this.list.splice(event, 1)
+
   }
 }
-
